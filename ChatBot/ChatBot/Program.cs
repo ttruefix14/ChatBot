74f2b37251c52e1e53a8e63e421f8a6bb2b667ad
ttruefix14 <ttruefix14@gmail.com>
2023-11-27 12:54:09 +0300

    Добавил проект ChatBot

---
+++ ChatBot/ChatBot/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tutor engTutor = new Tutor();
+
+            string word;
+            string translate;
+            engTutor.AddWord("dog", "собака");
+            engTutor.AddWord("cat", "кошка");
+            engTutor.AddWord("bird", "птица");
+            engTutor.AddWord("whale", "кит");
+
+            CheckWordResult result;
+            while (true)
+            {
+                word = engTutor.GetRandomEngWord();
+
+                Console.Write($"Переведите слово {word}: ");
+                translate = Console.ReadLine();
+                result = engTutor.CheckWord(word, translate);
+
+                switch (result)
+                {
+                    case CheckWordResult.Incorrect:
+                        Console.WriteLine($"Правильный ответ: \"{engTutor.Translate(word)}\"");
+                        break;
+                    case CheckWordResult.Correct:
+                        Console.WriteLine("Верно!");
+                        break;
+                    case CheckWordResult.Unknown:
+                        Console.WriteLine("Неизвестное слово");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ChatBot/ChatBot/Tutor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot
+{
+    class Tutor
+    {
+        private Random _random = new Random();
+        private Dictionary<string, string> _words = new Dictionary<string, string>();
+        public Dictionary<string, string> Words { get { return _words; } }
+        public void AddWord(string word, string translate)
+        {
+            _words.Add(word, translate);
+        }
+
+        public CheckWordResult CheckWord(string word, string translate)
+        {
+            if (!_words.ContainsKey(word))
+            {
+                return CheckWordResult.Unknown;
+            }
+            else if (_words[word].ToLower() == translate.ToLower())
+            {
+                return CheckWordResult.Correct;
+            }
+            else
+            {
+                return CheckWordResult.Incorrect;
+            }
+        }
+        public string Translate(string word)
+        {
+            if (_words.ContainsKey(word))
+            {
+                return _words[word];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public string GetRandomEngWord()
+        {
+            if (_words.Count == 0)
+            {
+                return null;
+            }
+            string randomWord = _words.Keys.ElementAt<string>(_random.Next(0, _words.Count));
+            return randomWord;
+        }
+    }
+}
